798e7e861bcd3e3efa947291482112c9318b98c7
Wethrift <37316429+wethrift@users.noreply.github.com>
2019-02-06 11:04:53 +1000

    Clear old session

--- middleware/withShop.js
+++ middleware/withShop.js
@@ -3,6 +3,12 @@ const {TEST_COOKIE_NAME, TOP_LEVEL_OAUTH_COOKIE_NAME} = require('../constants');
 module.exports = function withShop({ authBaseUrl } = {}) {
   return function verifyRequest(request, response, next) {
     const { query: { shop }, session, baseUrl } = request;
+    
+    // clear old sessions
+    if(shop && session.shop && shop != session.shop){
+      request.session.shop = null
+      request.session.accessToken = null
+    }
 
     if (session && session.accessToken) {
       response.cookie(TOP_LEVEL_OAUTH_COOKIE_NAME);
